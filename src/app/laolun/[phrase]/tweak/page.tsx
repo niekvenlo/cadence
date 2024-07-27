19f7b50cd1b16bd0f56a207113cc291a1a5a63dd
Nick Beaujean <niekvenlo@gmail.com>
2024-07-27 11:50:07 +0200

    dd

--- src/app/laolun/[phrase]/tweak/page.tsx
+++ src/app/laolun/[phrase]/tweak/page.tsx
@@ -12,7 +12,7 @@ import {
 import "../../style.css";
 import { getIsChinese } from "../../util";
 import { cx } from "../../../utils";
-import { updateLabel } from "../../phrase-actions-async";
+import { updateLabel, writePhrase } from "../../phrase-actions-async";
 import { useRouter } from "next/navigation";
 import NoSSR from "../../../components/NoSSR";
 
@@ -42,25 +42,46 @@ export default function Chinese({ params }: { params: { phrase: string } }) {
         />
       </div>
 
-      <PhraseEditor parts={parts} />
+      <PhraseEditor label={label} parts={parts} />
     </main>
   );
 }
 
-function PhraseEditor({ parts }) {
-  const [x, setX] = useState(parts);
+function PhraseEditor({ label, parts }) {
+  const setPartInColumnByIdx = (idx, part) => {
+    writePhrase({ label, parts: parts.toSpliced(idx, 1, part) });
+  };
+  const deleteColumnByIdx = (idx) => {
+    writePhrase({ label, parts: parts.toSpliced(idx, 1) });
+  };
+  const addSegmentToColumnByIdx = (idx, segment) => {
+    const newParts = [...parts];
+    newParts[idx] = [...newParts[idx], segment];
+    writePhrase({ label, parts: newParts });
+  };
+  const dragColumnByIdx = (idx, chunk) => {
+    const newParts = [...parts];
+    if (chunk % 1 === 0) {
+      [newParts[idx], newParts[idx + chunk]] = [
+        newParts[idx + chunk],
+        newParts[idx],
+      ];
+    } else {
+      const partToMove = newParts.splice(idx, 1)[0];
+      newParts.splice(idx + Math.floor(chunk), 0, partToMove);
+    }
+    writePhrase({ label, parts: newParts });
+  };
   return (
     <div className="editor">
-      {x.map((part, i) => (
+      {parts.map((part, i) => (
         <Column
-          key={i}
+          key={part.join() + i}
           part={part}
-          setPart={(n) =>
-            setX((x) => {
-              x[i] = n;
-              return [...x];
-            })
-          }
+          setPart={(n) => setPartInColumnByIdx(i, n)}
+          deleteColumn={() => deleteColumnByIdx(i)}
+          addSegmentToColumn={(segment) => addSegmentToColumnByIdx(i, segment)}
+          dragColumn={(chunkMoved) => dragColumnByIdx(i, chunkMoved)}
         />
       ))}
     </div>
@@ -74,7 +95,13 @@ const splitPartStringIntoPart = (string) =>
     .filter(getIsChinese)
     .map((f) => f.trim());
 
-const Column = ({ part, setPart }) => {
+const Column = ({
+  part,
+  setPart,
+  deleteColumn,
+  addSegmentToColumn,
+  dragColumn,
+}) => {
   const isWide = Math.max(...part.map((p) => p.length)) > 3;
   const isEmpty = part.length < 1;
   return (
@@ -83,30 +110,31 @@ const Column = ({ part, setPart }) => {
         <TextArea
           value={part.join("\n")}
           onChange={(e) => setPart(splitPartStringIntoPart(e.target.value))}
+          onDragGetChunk={dragColumn}
         />
-        <div className="overlay">
-          {part.map((segment) => (
-            <div
-              key={segment}
-              className={cx("line", `offset-${segment.length}`)}
-            >
-              <span className="pinyin">{pinyin[segment]}</span>
-              <RaritySpan segment={segment} />
-            </div>
-          ))}
-        </div>
-        <Suggestions part={part} />
-        {isEmpty && (
-          <div className="delete-column-overlay">
-            <div>
-              <button>Delete</button>
-            </div>
+        <PinyinOverlay part={part} />
+        <Suggestions part={part} addSegmentToColumn={addSegmentToColumn} />
+
+        <div className="delete-column-overlay">
+          <div>
+            <button onClick={deleteColumn}>Delete</button>
           </div>
-        )}
+        </div>
       </div>
     </>
   );
 };
+
+const PinyinOverlay = ({ part }) => (
+  <div className="overlay">
+    {part.map((segment) => (
+      <div key={segment} className={cx("line", `offset-${segment.length}`)}>
+        <span className="pinyin">{pinyin[segment]}</span>
+        <RaritySpan segment={segment} />
+      </div>
+    ))}
+  </div>
+);
 
 const RaritySpan = ({ segment }) => {
   const totalCount =
@@ -124,7 +152,7 @@ const RaritySpan = ({ segment }) => {
   );
 };
 
-const Suggestions = ({ part }) => {
+const Suggestions = ({ part, addSegmentToColumn }) => {
   const suggestions = getSuggested(part);
   if (suggestions.length < 1) {
     return null;
@@ -135,7 +163,9 @@ const Suggestions = ({ part }) => {
         <p>Quick add</p>
         <div>
           {suggestions.map((s) => (
-            <button key={s}>{s}</button>
+            <button key={s} onClick={() => addSegmentToColumn(s)}>
+              {s}
+            </button>
           ))}
         </div>
       </div>
@@ -148,11 +178,17 @@ const TextArea = ({
   className = "",
   onChange,
   onPaste = (e: Event) => {},
+  onDragGetChunk = (e: number) => {},
 }) => {
+  // draggable
+  const [isDraggable, setIsDraggable] = useState(false);
+
   // We don't want onChange to trigger while we are in composition mode.
   const preventOnChange = useRef(false);
   // We want to call handlePaste once the value has been updated.
   const justPasted = useRef(false);
+  const dragStartX = useRef<number | null>(null);
+  const draggableTimer = useRef<NodeJS.Timeout | null>(null);
 
   const handleChange = (e) => {
     if (justPasted.current === true) {
@@ -167,14 +203,33 @@ const TextArea = ({
   const handlePaste = (e) => {
     onPaste(e);
   };
+  const handleDoubleClick = () => {
+    setIsDraggable(true);
+    draggableTimer.current = setTimeout(() => setIsDraggable(false), 1000);
+  };
   return (
     <textarea
+      draggable={isDraggable}
       className={className}
       defaultValue={value}
       onCompositionStart={() => (preventOnChange.current = true)}
       onCompositionEnd={() => (preventOnChange.current = false)}
       onPaste={() => (justPasted.current = true)}
       onChange={handleChange}
+      onDragStart={(c) => {
+        dragStartX.current = c.screenX;
+        if (draggableTimer.current) {
+          clearTimeout(draggableTimer.current);
+        }
+      }}
+      onDragEnd={(c) => {
+        const xMovement = c.screenX - dragStartX.current!;
+        dragStartX.current = null;
+        const xMovementInChunks = Math.round(xMovement / 80) / 2;
+        onDragGetChunk(xMovementInChunks);
+        setIsDraggable(false);
+      }}
+      onDoubleClick={handleDoubleClick}
     />
   );
 };
